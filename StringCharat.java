05e93157dd534a6f2bea45cff1c7b86ec4abdd34
Srajal-Shrestha <srajalpradhan25@gmail.com>
2026-06-10 18:52:15 +0545

    Check the position of string in java 'CharAt used'

---
+++ StringCharat.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class StringCharat {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the position:");
+        int n = scanner.nextInt();
+
+    String name = "Russle";
+    System.out.println(name.charAt(n));
+
+    }
+}
