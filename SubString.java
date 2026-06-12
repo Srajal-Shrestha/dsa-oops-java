3e10b5cbcfc9fe19de0efb1cf4c58519592030bb
Srajal-Shrestha <srajalpradhan25@gmail.com>
2026-06-12 21:46:07 +0545

    Substring Concept in java.

---
+++ SubString.java
@@ -0,0 +1,9 @@
+public class SubString {
+    public static void main(String[] args0){
+        //String
+        //substring
+        String name = ("Aman and Anup");
+        System.out.println(name.substring(0, 4));
+
+    }
+}
