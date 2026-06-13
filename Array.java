257f1aea2e543b266e5b6c7399368469b34dd30c
Srajal-Shrestha <srajalpradhan25@gmail.com>
2026-06-13 22:21:34 +0545

    Array concept in java learn source-Apna Collage

---
+++ Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String[] args){
+        int physics = 87;
+        int maths = 97;
+        int chem = 96;
+
+        int[] marks = new int[3];
+        marks[0] = 87;
+        marks[1] = 97;
+        marks[2] = 96;
+        System.out.println(marks[2]);
+        
+    }
+}
