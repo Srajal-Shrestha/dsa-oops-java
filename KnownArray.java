b71f7922acba28eb801c44c8fdc2fe2428ddf75e
Srajal-Shrestha <srajalpradhan25@gmail.com>
2026-06-16 16:30:27 +0545

    Array print from 2 dimentional Arrays!!

---
+++ KnownArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class KnownArray {
+    public static void main(String[] args) {
+        int[] marks = {97, 76, 89};
+        int[][] finalmarks = {{97, 76, 89}, {76, 89, 97}}; 
+        System.out.println(finalmarks[1][0]);
+
+    }
+}
