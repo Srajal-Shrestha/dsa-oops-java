b229b00f09c1ac3726d980eafabb1233211a6c37
Srajal-Shrestha <srajalpradhan25@gmail.com>
2026-06-16 17:06:33 +0545

    Casting concept

---
+++ Casting.java
@@ -0,0 +1,12 @@
+public class Casting {
+    public static void main(String[] args) {
+        //Casting
+        //double price = 2300;
+        //double finalprice  = price + 18;
+        ///System.out.println(finalprice);
+
+        int price = 100;
+        int finalprice = price + (int)18.99;
+        System.out.println(finalprice);
+    }
+}
