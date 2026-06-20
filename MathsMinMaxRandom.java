1a8b6abc62b014bc641d99cad99f96944ee807e5
Srajal-Shrestha <srajalpradhan25@gmail.com>
2026-06-20 23:01:05 +0545

    learn concept of printing max min and random value with additional concept of casting

---
+++ MathsMinMaxRandom.java
@@ -0,0 +1,14 @@
+public class MathsMinMaxRandom {
+    public static void main(String[] args) {
+    //MATH
+    //MIN
+     //   System.out.println(Math.min(6,7));
+     //MAX
+     //   System.out.println(Math.max(6,7));
+     //RANDOM
+      //System.out.println(Math.random());
+      //it will print random value below 1 not negative in the form of long now lets print in integer value by explicit casting
+       System.out.println((int)(Math.random() * 100));
+       //It will print out different random values by using random
+    }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,7 +1,7 @@
-public class ApnaCollage{
-
-public static void main(String args[])
-{
-    System.out.println("Hello, This's my DSA journey");
-}
+public class MathsMinMaxRandom {
+    public static void main(String[] args) {
+    //MATH
+    //MIN
+        System.out.println(Math.min(6,7));
+    }
 }
